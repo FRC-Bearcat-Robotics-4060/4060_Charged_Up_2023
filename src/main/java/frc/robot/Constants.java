a244591c8e9a18f07a37e155d9a165d7fa772b09
Jason Smith <jason.inet@gmail.com>
2023-03-03 18:37:10 -0800

    Update steer offsets

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -52,13 +52,13 @@ public final class Constants {
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 7;
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 13;
     // public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(38.1);//(65.039);
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(38.1);
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 8;
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 3;
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12;
     // public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(104);
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(104.0);
 
     public static final int ARM_MOTOR_ID = 9;
     public static final int WRIST_SERVO_ID = 0;
