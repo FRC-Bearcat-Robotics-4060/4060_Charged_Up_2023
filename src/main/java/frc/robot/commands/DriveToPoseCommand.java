8aa0f7b0c47f97f29ee35504fa7381be3236e4c1
Jason Smith <jason.inet@gmail.com>
2023-03-07 21:32:13 -0800

    Adjust DriveToPose paramters to make it more likely to reach target

--- src/main/java/frc/robot/commands/DriveToPoseCommand.java
+++ src/main/java/frc/robot/commands/DriveToPoseCommand.java
@@ -16,16 +16,16 @@ import frc.robot.subsystems.DrivetrainSubsystem;
  */
 public class DriveToPoseCommand extends CommandBase {
   
-  private static final double TRANSLATION_TOLERANCE = 0.1;
+  private static final double TRANSLATION_TOLERANCE = 0.25;
   private static final double THETA_TOLERANCE = Units.degreesToRadians(0.5);
 
-  private static final TrapezoidProfile.Constraints X_CONSTRAINTS = new TrapezoidProfile.Constraints(1, 1);
-  private static final TrapezoidProfile.Constraints Y_CONSTRAINTS = new TrapezoidProfile.Constraints(1, 1);
+  private static final TrapezoidProfile.Constraints X_CONSTRAINTS = new TrapezoidProfile.Constraints(1, 2);
+  private static final TrapezoidProfile.Constraints Y_CONSTRAINTS = new TrapezoidProfile.Constraints(1, 2);
   private static final TrapezoidProfile.Constraints OMEGA_CONSTRAINTS =   new TrapezoidProfile.Constraints(8, 8);
 
-  private final ProfiledPIDController xController = new ProfiledPIDController(3, 0, 0, X_CONSTRAINTS);
-  private final ProfiledPIDController yController = new ProfiledPIDController(3, 0, 0, Y_CONSTRAINTS);
-  private final ProfiledPIDController thetaController = new ProfiledPIDController(2, 0, 0, OMEGA_CONSTRAINTS);
+  private final ProfiledPIDController xController = new ProfiledPIDController(3, 0.5, 0, X_CONSTRAINTS);
+  private final ProfiledPIDController yController = new ProfiledPIDController(3, 0.5, 0, Y_CONSTRAINTS);
+  private final ProfiledPIDController thetaController = new ProfiledPIDController(2, 0.5, 0, OMEGA_CONSTRAINTS);
 
   private final DrivetrainSubsystem drivetrainSubsystem;
   private final Supplier<Pose2d> poseProvider;
@@ -49,6 +49,7 @@ public class DriveToPoseCommand extends CommandBase {
 
   @Override
   public void initialize() {
+    System.out.println("DTP: Init");
     var robotPose = poseProvider.get();
     thetaController.reset(robotPose.getRotation().getRadians());
     xController.reset(robotPose.getX());
@@ -96,6 +97,7 @@ public class DriveToPoseCommand extends CommandBase {
 
   @Override
   public void end(boolean interrupted) {
+    System.out.println("DTP: End");
     drivetrainSubsystem.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
   }
 
