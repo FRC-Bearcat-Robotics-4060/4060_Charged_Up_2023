93cac62204cdc063a30a65f00aa8b4b8d58fcbcf
Jason Smith <jason.inet@gmail.com>
2023-03-07 20:57:55 -0800

    Update drive constants for competition robot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -26,39 +26,43 @@ public final class Constants {
      *
      * Should be measured from center to center.
      */
-    public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.6223;
+    public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.473075;
     /**
      * The front-to-back distance between the drivetrain wheels.
      *
      * Should be measured from center to center.
      */
-    public static final double DRIVETRAIN_WHEELBASE_METERS = 0.6223;
+    public static final double DRIVETRAIN_WHEELBASE_METERS = 0.473075;
 
     // public static final int DRIVETRAIN_PIGEON_ID = 0; // Set Pigeon ID
 
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
-    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2;
-    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 12;
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(355.0);
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 5;
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 11;
+    // public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(230.625);
 
-    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 7;
-    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 8;
-    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 13;
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(44.0);
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 6;
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 2;
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 10;
+    // public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(196.875);
 
-    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 3;
-    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 4;
-    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 11;
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(329.8);
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 4;
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 7;
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 13;
+    // public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(259.8);
 
-    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 5;
-    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 6;
-    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 10;
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(27.2);
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 8;
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 3;
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12;
+    // public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(104.15);
 
-    public static final int ARM_MOTOR_ID = 15;
-    public static final int WRIST_SERVO_ID = 1;
-    public static final int HAND_SERVO_ID = 2;
+    public static final int ARM_MOTOR_ID = 9;
+    public static final int WRIST_SERVO_ID = 0;
+    public static final int HAND_SERVO_ID = 1;
     
     public static final SwerveDriveKinematics KINEMATICS = new SwerveDriveKinematics(
         // Front left
