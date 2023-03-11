64a0e30d0558799c8b2a9da013e9f05b40ab9f09
Jason Smith <jason.inet@gmail.com>
2023-03-11 15:13:14 -0800

    Re-zero encoders
    (cherry picked from commit 4a23fa4c98f3b976240758917143d7d6ee132ef0)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -40,25 +40,26 @@ public final class Constants {
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 5;
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 11;
     // public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(230.625);
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(228.95);
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 6;
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 2;
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 10;
     // public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(196.875);
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(197.3);
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 4;
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 7;
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 13;
     // public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(259.8);
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(259.6);
 
+    // This motor is probably not Loctited...but we can't get the magnet out.
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 8;
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 3;
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12;
     // public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(104.15);
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(105.64);
 
     public static final int ARM_MOTOR_ID = 9;
     public static final int WRIST_SERVO_ID = 0;
